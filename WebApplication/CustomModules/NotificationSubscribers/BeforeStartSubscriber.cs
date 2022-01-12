73d746bb4beb38b4212d004f669ae789932e42ce
Mads Agergaard <maj@novicell.dk>
2022-01-12 08:03:08 +0100

    Add project files.

---
+++ WebApplication/CustomModules/NotificationSubscribers/BeforeStartSubscriber.cs
@@ -0,0 +1,42 @@
+﻿using Dynamicweb.Extensibility.Notifications;
+2using Dynamicweb.Notifications;
+3using System.Configuration;
+4using System.Data.SqlClient;
+5using System.Web;
+6using System.Xml;
+7
+8namespace WebApplication.CustomModules.NotificationSubscribers.Application
+9{
+10    [Subscribe(Standard.Application.BeforeStart)]
+11    public class BeforeStartSubscriber : NotificationSubscriber
+12    {
+13        public override void OnNotify(string notification, NotificationArgs args)
+14        {
+15            if (args == null)
+16                return;
+17
+18            if (!(args is Standard.Application.BeforeStartArgs))
+19                return;
+20
+21            var connectionString = ConfigurationManager.ConnectionStrings["WebsiteDb"].ConnectionString;
+22            var decoder = new SqlConnectionStringBuilder(connectionString);
+23
+24            var doc = new XmlDocument();
+25            var gbSettings = HttpContext.Current.Server.MapPath("~/Files/GlobalSettings.aspx");
+26
+27            doc.Load(gbSettings);
+28
+29            doc.SelectSingleNode("/Globalsettings/System/Database/Password").InnerText = decoder.Password;
+30            doc.SelectSingleNode("/Globalsettings/System/Database/UserName").InnerText = decoder.UserID;
+31            doc.SelectSingleNode("/Globalsettings/System/Database/Database").InnerText = decoder.InitialCatalog;
+32            doc.SelectSingleNode("/Globalsettings/System/Database/SQLServer").InnerText = decoder.DataSource;
+33
+34            doc.Save(gbSettings);
+35
+36            if (Dynamicweb.Configuration.SystemConfiguration.Instance != null)
+37            {
+38                Dynamicweb.Configuration.SystemConfiguration.Reset();
+39            }
+40        }
+41    }
+42}
---
+++ WebApplication/Files/Templates/Designs/Swift/Assets/_src/js/swift.js
@@ -0,0 +1,38 @@
+import * as bootstrap from 'bootstrap';
+import { Sliders } from './_sliders';
+import { Favorites } from './_favorites';
+import { Cart } from './_cart';
+import { Scroll } from './_scroll';
+import { ProductList } from './_productlist';
+import { PageUpdater } from './_pageupdater';
+import { LocationsMap } from './_locationsmap';
+import { VariantSelector } from './_variantselector';
+import { Video } from './_video';
+import { Typeahead } from './_typeahead';
+
+//Bootstrap
+window.bootstrap = bootstrap;
+
+//Swift modules
+const swift = function () {
+	return {
+		Cart: Cart,
+		Sliders: Sliders,
+		Favorites: Favorites,
+		Scroll: Scroll,
+		ProductList: ProductList,
+		PageUpdater: PageUpdater,
+		LocationsMap: LocationsMap,
+		VariantSelector: VariantSelector,
+		Typeahead: Typeahead,
+		Video: Video
+	}
+}();
+export { swift };
+
+window.swift = swift;
+
+//Popstate
+window.onpopstate = function (event) {
+	swift.Typeahead.navigateToPage(document.location.href);
+};
